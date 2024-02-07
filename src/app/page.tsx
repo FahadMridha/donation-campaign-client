fbf383b40cdb199efd318c36c665559d69875396
fahad Mridha <sohelcse16@gmail.com>
2024-02-08 03:13:05 +0600

    hero section added  with banner image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Hero from "@/components/ui/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-    <h1>Welcome to Donation Club</h1></>
+    <h1>Welcome to Donation Club</h1>
+    <Hero/>
+    </>
   );
 }
